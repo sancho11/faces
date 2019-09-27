5715a4373f0883740031be09ec2fb5c3a0483333
Enmanuel Sancho Quintanilla <emmanuel.sancho11@hotmail.com>
2019-09-26 22:02:05 -0600

    Creo que ahora si

--- faces.py
+++ faces.py
@@ -1,24 +1,66 @@
-#!/usr/bin/env python3
-import sys
-import cv2
 import numpy as np
+import cv2
 import pickle
+import time
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
 
 
-vid_cam = cv2.VideoCapture(0)
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-face_id = 1
-while(vid_cam.isOpened()):
-    ret, image_frame = vid_cam.read()
-    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        font= cv2.FONT_HERSHEY_SIMPLEX
-        name="Persona"
-        cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
-        cv2.putText(image_frame,name, (x,y), font,1,(255,0,0), 2, cv2.LINE_AA)
-        cv2.imshow('frame', image_frame)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizers/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+start = time.time()
+seconds = 0
+contfps=0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+    	#print(x,y,w,h)
+    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+    	roi_color = frame[y:y+h, x:x+w]
+
+    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+    	id_, conf = recognizer.predict(roi_gray)
+    	if conf>=4 and conf <= 85:
+    		#print(5: #id_)
+    		#print(labels[id_])
+    		font = cv2.FONT_HERSHEY_SIMPLEX
+    		name = "Persona"
+    		color = (255, 255, 255)
+    		stroke = 2
+    		cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+    	img_item = "7.png"
+    	cv2.imwrite(img_item, roi_color)
+
+    	color = (255, 0, 0) #BGR 0-255 
+    	stroke = 2
+    	end_cord_x = x + w
+    	end_cord_y = y + h
+    	cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    	#subitems = smile_cascade.detectMultiScale(roi_gray)
+    	#for (ex,ey,ew,eh) in subitems:
+    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-vid_cam.release()
+
+# When everything done, release the capture
+end = time.time()
+seconds = end - start
+print("fps:")
+print(contfps/seconds)
+cap.release()
 cv2.destroyAllWindows()
